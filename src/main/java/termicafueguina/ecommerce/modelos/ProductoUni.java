dd63ab7248e844c349593879bae5a9438437e880
Agustin Martinez <gutin97agustin.martinez@gmail.com>
2024-02-13 15:36:52 -0300

    agregado getters y setters producto por unidad

--- src/main/java/termicafueguina/ecommerce/modelos/ProductoUni.java
+++ src/main/java/termicafueguina/ecommerce/modelos/ProductoUni.java
@@ -46,6 +46,40 @@ public class ProductoUni {
         this.esPorPeso = false;
     }
 
+    //GETTERS
+    public long getId() {return id;}
+    public String getNombre() {return nombre;}
+    public TipoProducto getTipo() {return tipo;}
+    public String getDescripcion() {return descripcion;}
+    public int getStock() {return stock;}
+    public double getPrecio() {return precio;}
+    public PaisProducto getOrigen() {return origen;}
+    public Set<OrdenProductoUni> getOrdenProductoUnis() {return ordenProductoUnis;}
+    public List<Double> getPuntuaciones() {return puntuaciones;}
+    public String getUrl() {return url;}
+    public double getDescuento() {return descuento;}
+    public boolean getEstaActivo() {return estaActivo;}
+    public boolean esPorPeso() {return esPorPeso;}
+    //SETTERS
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    public void setTipo(TipoProducto tipo) {this.tipo = tipo;}
+    public void setDescripcion(String descripcion) {this.descripcion = descripcion;}
+    public void setStock(int stock) {this.stock = stock;}
+    public void setPrecio(double precio) {this.precio = precio;}
+    public void setOrigen(PaisProducto origen) {this.origen = origen;}
+    public void setOrdenProductoUnis(Set<OrdenProductoUni> ordenProductoUnis) {this.ordenProductoUnis = ordenProductoUnis;}
+    public void setPuntuaciones(List<Double> puntuaciones) {this.puntuaciones = puntuaciones;}
+    public void setUrl(String url) {this.url = url;}
+    public void setDescuento(double descuento) {this.descuento = descuento;}
+    public void setEstaActivo(boolean estaActivo) {this.estaActivo = estaActivo;}
 
+    //ADDERS
+    public void addOrdenProductoUni(OrdenProductoUni ordenProductoUni) {
+        ordenProductoUni.setProductoUni(this);
+        ordenProductoUnis.add(ordenProductoUni);
+    }
+    public void addPuntuacion (double valor){
+        puntuaciones.add(valor);
+    }
 
 }
