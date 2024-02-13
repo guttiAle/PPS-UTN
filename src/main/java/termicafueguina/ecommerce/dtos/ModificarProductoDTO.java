e5d985f5913b465173db044e23ba1b26c75743ee
Agustin Martinez <gutin97agustin.martinez@gmail.com>
2024-02-13 16:08:33 -0300

    creacion DTO para modificación de productos desde ADMIN

---
+++ src/main/java/termicafueguina/ecommerce/dtos/ModificarProductoDTO.java
@@ -0,0 +1,22 @@
+package termicafueguina.ecommerce.dtos;
+
+public class ModificarProductoDTO {
+    private String nombre;
+    private String nuevoNombre;
+    private String tipo;
+    private String descripcion;
+    private String stock;
+    private String precio;
+    private String origen;
+    private String url;
+
+    //GETTERS
+    public String getNombre() {return nombre;}
+    public String getNuevoNombre() {return nuevoNombre;}
+    public String getTipo() {return tipo;}
+    public String getDescripcion() {return descripcion;}
+    public String getStock() {return stock;}
+    public String getPrecio() {return precio;}
+    public String getOrigen() {return origen;}
+    public String getUrl() {return url;}
+}
