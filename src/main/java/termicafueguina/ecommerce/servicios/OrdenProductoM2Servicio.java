e54f90abc6a3835c51fa5be10f1539baf06f8097
Agustin Martinez <gutin97agustin.martinez@gmail.com>
2024-02-13 16:26:04 -0300

    interfaz servicio orden productom2"

---
+++ src/main/java/termicafueguina/ecommerce/servicios/OrdenProductoM2Servicio.java
@@ -0,0 +1,6 @@
+package termicafueguina.ecommerce.servicios;
+import termicafueguina.ecommerce.modelos.OrdenProductoPeso;
+
+public interface OrdenProductoPesoServicio {
+    void saveOrdenProductoPeso(OrdenProductoPeso ordenProductoPeso);
+}
