4e65399051a27636aec6cafef2cc5d03dd47b790
Agustin Martinez <gutin97agustin.martinez@gmail.com>
2024-02-13 15:14:30 -0300

    initial scaffolding for ecommerce

---
+++ src/test/java/termicafueguina/ecommerce/ECommercetermicaFueguinaApplicationTests.java
@@ -0,0 +1,13 @@
+package termicafueguina.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceTermicaFueguinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
