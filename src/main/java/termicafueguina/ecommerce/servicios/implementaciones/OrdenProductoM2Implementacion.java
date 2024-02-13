07a57a50ee75a370b158374b5c6c7db23a252275
Agustin Martinez <gutin97agustin.martinez@gmail.com>
2024-02-13 16:28:40 -0300

    refactor implementacion ordenproductom2

---
+++ src/main/java/termicafueguina/ecommerce/servicios/implementaciones/OrdenProductoM2Implementacion.java
@@ -0,0 +1,19 @@
+package termicafueguina.ecommerce.servicios.implementaciones;
+
+import termicafueguina.ecommerce.modelos.OrdenProductoM2;
+import termicafueguina.ecommerce.repositorios.OrdenProductoM2Repositorio;
+import termicafueguina.ecommerce.servicios.OrdenProductoM2Servicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrdenProductoM2Implementacion implements OrdenProductoM2Servicio {
+
+    @Autowired
+    private OrdenProductoM2Repositorio ordenProductoPesoRepositorio;
+
+    @Override
+    public void saveOrdenProductoPeso(OrdenProductoM2 ordenProductoPeso) {
+        ordenProductoPesoRepositorio.save(ordenProductoPeso);
+    }
+}
