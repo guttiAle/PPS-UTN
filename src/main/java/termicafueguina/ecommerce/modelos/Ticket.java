a3cfa75e9c300ac16e5ddfdffb49cc87464d838b
Agustin Martinez <gutin97agustin.martinez@gmail.com>
2024-02-13 15:56:03 -0300

    creacion modelo base para ticket de compra

---
+++ src/main/java/termicafueguina/ecommerce/modelos/Ticket.java
@@ -0,0 +1,41 @@
+package termicafueguina.ecommerce.modelos;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String numero;
+
+    @OneToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name = "ticket")
+    private Orden orden;
+
+    //CONSTRUCTORES
+    public Ticket(){}
+
+    public Ticket(String numero) {
+        this.numero = numero;
+    }
+
+    //GETTERS
+    public long getId() {return id;}
+    public String getNumero() {return numero;}
+
+    public Orden getOrden() {return orden;}
+
+    //SETTERS
+    public void setNumero(String numero) {this.numero = numero;}
+    public void setOrden(Orden orden) {this.orden = orden;}
+
+    //ADDERS
+    public void addOrden(Orden orden) {
+        orden.setTicket(this);
+        this.orden = orden;
+    }
+}
