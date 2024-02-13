cda6747799d93ce09f4dd74d52db2ebdcb72ae86
Agustin Martinez <gutin97agustin.martinez@gmail.com>
2024-02-13 16:29:17 -0300

    interfaz servicio productouni

---
+++ src/main/java/termicafueguina/ecommerce/servicios/OrdenProductoUniServicio.java
@@ -0,0 +1,7 @@
+package termicafueguina.ecommerce.servicios;
+
+import termicafueguina.ecommerce.modelos.OrdenProductoUni;
+
+public interface OrdenProductoUniServicio {
+    void saveOrdenProductoUni(OrdenProductoUni ordenProductoUni);
+}
