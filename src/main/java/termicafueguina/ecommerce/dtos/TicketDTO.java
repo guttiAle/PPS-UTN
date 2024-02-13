c801decf0c08a3aa515a95b2165ffa4b9a0e4d0b
Agustin Martinez <gutin97agustin.martinez@gmail.com>
2024-02-13 16:14:04 -0300

    DTO para creación de ticket

---
+++ src/main/java/termicafueguina/ecommerce/dtos/TicketDTO.java
@@ -0,0 +1,24 @@
+package termicafueguina.ecommerce.dtos;
+
+import termicafueguina.ecommerce.modelos.Ticket;
+
+public class TicketDTO {
+    private long id;
+    private String numero;
+    private String nombreDueñoTicket;
+    private String apellidoDueñoTicket;
+
+    //CONSTRUCTOR
+    public TicketDTO(Ticket ticket) {
+        this.id = ticket.getId();
+        this.numero = ticket.getNumero();
+        this.nombreDueñoTicket = ticket.getOrden().getClient().getNombre();
+        this.apellidoDueñoTicket = ticket.getOrden().getClient().getApellido();
+    }
+
+    //GETTERS
+    public long getId() {return id;}
+    public String getNumero() {return numero;}
+    public String getNombreDueñoTicket() {return nombreDueñoTicket;}
+    public String getApellidoDueñoTicket() {return apellidoDueñoTicket;}
+}
