a2e21c4caa0bf36cf6466059836bc6cc4209bc26
Agustin Martinez <gutin97agustin.martinez@gmail.com>
2024-02-13 16:21:11 -0300

    creacion de asignador de roles en autorizadorweb

--- src/main/java/termicafueguina/ecommerce/configuraciones/AutorizacionWeb.java
+++ src/main/java/termicafueguina/ecommerce/configuraciones/AutorizacionWeb.java
@@ -25,8 +25,35 @@ public class AutorizacionWeb {
                 .antMatchers(HttpMethod.POST,"/api/logout", "/api/login").permitAll()
                 .antMatchers("/rest", "/rest/**").permitAll()
 
+//                CLIENT
+                .antMatchers("/api/ordenes-cliente").hasAuthority("CLIENTE")
 
+//                ADMIN
+
+                .antMatchers("/api/clientes").hasAuthority("ADMINISTRADOR");
+
+
+        //.anyRequest().denyAll();
+
+        http.formLogin()
+                .usernameParameter("email")
+                .passwordParameter("contrasena")
+                .loginPage("/api/login");
+        http.logout().logoutUrl("/api/logout").deleteCookies("JSESSIONID");
+
+        http.csrf().disable(); // turn off checking for CSRF tokens
+        http.headers().frameOptions().disable();// disabling frameOptions so h2-console can be accessed
+        http.exceptionHandling().authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));// if user is not authenticated, just send an authentication failure response
+        http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));// if login is successful, just clear the flags asking for authentication
+        http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));// if login fails, just send an authentication failure response
+        http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());// if logout is successful, just send a success response
 
         return http.build();
+    }
+    private void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+        }
     }
 }
